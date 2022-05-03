2589b11e3f38e724c13a66a214cbfa6c690f28fb
lauralaura12 <analaura04122000.com>
2022-05-03 09:38:35 -0500

    primer commit, Hola Mundo

---
+++ HolaMundo.java
@@ -0,0 +1,14 @@
+// Contreras Santiago Rosario  //
+// Chavez Jeronimo Diana Laura //
+// Garcia Gil Lizbeth          //
+
+package hola.mundo;
+
+public class HolaMundo {
+    
+    public static void main(String[] args){
+
+System.out.println(" HOLA MUNDO ");
+    }
+    
+}
